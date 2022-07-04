e943fb5af12516c9aa5ac189e82c2094b7507a4a
Mark Pavel <markpavel02@gmail.com>
2022-07-04 00:11:29 -0400

    Added Full Menu Functionality w/o Displaying Data

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
+const $detailed = $('#detailed');
+const $tabs= $('.tabs');
+
 window.scrollTo(0, 0);
 
-$(".tabs").click(event => {
+$tabs.click(event => {
     event.preventDefault();
 
     const target = $(event.target).parent('li');
     const sibling = $(event.target).parent('li').siblings('li');
-    const parent = $(target.parent().parent().parent());
 
     if(target.hasClass('is-active')) {
         target.removeClass('is-active').addClass('has-text-light');
 
-        checkContent(target);
-        checkContent(sibling);
+        checkTabs(target);
+        checkTabs(sibling);
 
         return;
     }
@@ -19,11 +21,38 @@ $(".tabs").click(event => {
     target.removeClass('has-text-light').addClass('is-active');
     sibling.removeClass('is-active').addClass('has-text-light');
 
-    checkContent(sibling);
-    checkContent(target);
+    checkTabs(sibling);
+    checkTabs(target);
 });
 
-function checkContent(target) {
+$detailed.on('click', 'a', event => {
+    event.preventDefault();
+
+    const target = $(event.target);
+    const content = $(event.target).siblings('ul');
+
+    if(target.parent().parent().hasClass('border-tertiary')) {
+        return;
+    }   
+
+    $detailed.find('.border-tertiary').each(function() {
+        $(this).addClass('gone');
+    });
+
+    if(target.hasClass('is-active')) {
+        target.removeClass('is-active');
+        content.addClass('gone');
+    } else {
+        $detailed.find('.is-active').each(function() {
+            $(this).removeClass('is-active');
+        });
+
+        target.addClass('is-active');
+        content.removeClass('gone');
+    }    
+})
+
+function checkTabs(target) {
     const content = $(
         $(target).attr('data-content')
     );
